53ac014566e474e60da0875e08edfe5ab55ed4cc
Antti Räsänen <143749752+Raezane@users.noreply.github.com>
2024-04-20 17:07:51 +0300

    create better initial program logic

--- javascript.js
+++ javascript.js
@@ -1,84 +1,70 @@
-const startGame = (function () {
+(function() {
 
-    const dialog = document.querySelector('dialog');
-    const close = document.querySelector('form > button');
+  const domElements = {
 
-    dialog.showModal();
-    close.addEventListener('click', initiateGame)
+    dialog : document.querySelector('dialog'),
+    close : document.querySelector('form > button'),
 
-})();
+    namePlayer1: document.querySelector('#firstplayerName'),
+    namePlayer2 : document.querySelector('#secondplayerName'),
 
+    firstplayer : document.querySelector('.leftside > p:first-child'),
+    secondplayer : document.querySelector('.rightside > p:first-child'),
 
-function interactables () {
+    cells : document.querySelectorAll('.cell')
+  };
 
-    const namePlayer1 = document.querySelector('#firstplayerName');
-    const namePlayer2 = document.querySelector('#secondplayerName');
+  const playerManagement = { 
 
-    const firstplayer = document.querySelector('.leftside > p:first-child');
-    const secondplayer = document.querySelector('.rightside > p:first-child');
+    playerOne: undefined,
+    playerTwo: undefined,
 
-    const cells = document.querySelectorAll('.cell');
+    createPlayers: function () {
+      this.playerOne = Player(domElements.namePlayer1.value, 'O');
+      this.playerTwo = Player(domElements.namePlayer2.value, 'X');
 
-    return {namePlayer1, namePlayer2, firstplayer, secondplayer, cells};
-};
+    },
 
+    addPlayerNames: function () {
+      domElements.firstplayer.textContent = domElements.namePlayer1.value,
+      domElements.secondplayer.textContent = domElements.namePlayer2.value
+    }
+  };
 
-function createPlayers() {
+  const gameBoard = {
 
-    const {firstplayer, secondplayer, namePlayer1, namePlayer2} = interactables()
+    board: [0,1,2,3,4,5,6,7,8], 
 
-    playerOne = Player(namePlayer1.value, 'O');
-    playerTwo = Player(namePlayer2.value, 'X');
+    getBoard: () => board,
+    setPlayerMark: function(mark) {
+      board[4] = mark;
+    }
+  };
 
-    firstplayer.textContent = namePlayer1.value;
-    secondplayer.textContent = namePlayer2.value;
+  const startGame = (function () {
 
-    return [playerOne, playerTwo];
+    domElements.dialog.showModal();
+    domElements.close.addEventListener('click', gameHandler);
 
-};
+  })();
 
-
-function initiateGame () {
+  function gameHandler () {
     
-    const {cells} = interactables();
+    playerManagement.createPlayers();
+    playerManagement.addPlayerNames();
+
+    const cells = domElements.cells;
 
     let indexnumber = 0
     cells.forEach((cell) => {
-        cell.setAttribute('cellIndex', indexnumber)
-        cell.addEventListener('click', checkIfEmpty);
-        indexnumber += 1;
+      cell.setAttribute('cellIndex', indexnumber)
+      cell.addEventListener('click', checkIfEmpty);
+      indexnumber += 1;
     });
 
-    
+  };
 
-};
-
-function checkIfEmpty () {
-
-    const players = createPlayers();
-    console.log(players)
-
-    let whoseTurn = players[0].getMark();
-
-    const board = [0,1,2,3,4,5,6,7,8];
-    let gamemark;
-    let clickedCell;
-
-    if (this.hasChildNodes() == false) {
-        let gamemark = document.createElement('p');
-        gamemark.textContent = 'O';
-        this.appendChild(gamemark);
-        clickedCell = board[this.getAttribute('cellIndex')];
-        //find corresponding clicked cell and board index/number and update gameboard with gamemark
-        if (this.getAttribute('cellIndex') == clickedCell) {
-            board[clickedCell] = whoseTurn;
-        };
-    };
-
-};
-
-
-function Player (name, mark) {
+  function Player (name, mark) {
     this.name = name;
     this.mark = mark;
 
@@ -89,53 +75,89 @@ function Player (name, mark) {
     const getScore = () => score;
 
     return {name, getMark, scoreUp, getScore};
-};
 
-function checkIfWon (whoseTurn) {
-    if (gameBoard.board[0] == whoseTurn && gameBoard.board[1] == whoseTurn && gameBoard.board[2] == whoseTurn) {
-        return true;
-    } else if (gameBoard.board[3] == whoseTurn && gameBoard.board[4] == whoseTurn && gameBoard.board[5] == whoseTurn) {
-        return true;
-    } else if (gameBoard.board[6] == whoseTurn && gameBoard.board[7] == whoseTurn && gameBoard.board[8] == whoseTurn) {
-        return true;
-    } else if (gameBoard.board[0] == whoseTurn && gameBoard.board[3] == whoseTurn && gameBoard.board[6] == whoseTurn) {
-        return true;
-    } else if (gameBoard.board[1] == whoseTurn && gameBoard.board[4] == whoseTurn && gameBoard.board[7] == whoseTurn) {
-        return true;
-    } else if (gameBoard.board[2] == whoseTurn && gameBoard.board[5] == whoseTurn && gameBoard.board[8] == whoseTurn) { 
-        return true;
-    } else if (gameBoard.board[0] == whoseTurn && gameBoard.board[4] == whoseTurn && gameBoard.board[8] == whoseTurn) { 
-        return true;
-    } else if (gameBoard.board[2] == whoseTurn && gameBoard.board[4] == whoseTurn && gameBoard.board[6] == whoseTurn) { 
-        return true;
+  };
+
+  function checkIfEmpty (e) {
+
+    console.log(playerManagement.playerOne);
+
+    console.log(board.getBoard());
+    const players = createPlayers();
+
+    console.log(players)
+
+    let whoseTurn = players[0].getMark();
+    board.setPlayerMark(whoseTurn);
+    console.log(board.getBoard());
+
+    let gamemark;
+    let clickedCell;
+
+    if (this.hasChildNodes() == false) {
+      let gamemark = document.createElement('p');
+      gamemark.textContent = whoseTurn;
+      this.appendChild(gamemark);
+      clickedCell = board[this.getAttribute('cellIndex')];
+      //find corresponding clicked cell and board index/number and update board with gamemark
+      if (this.getAttribute('cellIndex') == clickedCell) {
+        board[clickedCell] = whoseTurn;
+      };
     };
+  };
+
+
+  function checkIfWon (whoseTurn) {
+    if (board.board[0] == whoseTurn && board.board[1] == whoseTurn && board.board[2] == whoseTurn) {
+        return true;
+    } else if (board.board[3] == whoseTurn && board.board[4] == whoseTurn && board.board[5] == whoseTurn) {
+        return true;
+    } else if (board.board[6] == whoseTurn && board.board[7] == whoseTurn && board.board[8] == whoseTurn) {
+        return true;
+    } else if (board.board[0] == whoseTurn && board.board[3] == whoseTurn && board.board[6] == whoseTurn) {
+        return true;
+    } else if (board.board[1] == whoseTurn && board.board[4] == whoseTurn && board.board[7] == whoseTurn) {
+        return true;
+    } else if (board.board[2] == whoseTurn && board.board[5] == whoseTurn && board.board[8] == whoseTurn) { 
+        return true;
+    } else if (board.board[0] == whoseTurn && board.board[4] == whoseTurn && board.board[8] == whoseTurn) { 
+        return true;
+    } else if (board.board[2] == whoseTurn && board.board[4] == whoseTurn && board.board[6] == whoseTurn) { 
+        return true;
+      };
 
     return false;
-};
+  };
 
-const adsdsd = (function () {
+  /*
 
-    let whoseTurn = playerTwo.getMark();
+  const adsdsd = (function () {
 
-    gameBoard.board[4] = whoseTurn;
-    gameBoard.board[8] = whoseTurn;
-    gameBoard.board[0] = whoseTurn;
-    
-        gameBoard.showGameStatus();
-        
-        if (checkIfWon(whoseTurn)) {
-            switch (whoseTurn) {
-                case playerOne.getMark():
-                    console.log('PlayerOne wins!');
-                    break;
-                case playerTwo.getMark():
-                    console.log('PlayerTwo wins!');
-                    break;
-                };
-            };
-    
-    whoseTurn === playerOne.getMark() ? whoseTurn = playerTwo.getMark() : whoseTurn = playerOne.getMark();
+      let whoseTurn = playerTwo.getMark();
 
-    return {playerOne, playerTwo}
+      board.board[4] = whoseTurn;
+      board.board[8] = whoseTurn;
+      board.board[0] = whoseTurn;
+      
+          board.showGameStatus();
+          
+          if (checkIfWon(whoseTurn)) {
+              switch (whoseTurn) {
+                  case playerOne.getMark():
+                      console.log('PlayerOne wins!');
+                      break;
+                  case playerTwo.getMark():
+                      console.log('PlayerTwo wins!');
+                      break;
+                  };
+              };
+      
+      whoseTurn === playerOne.getMark() ? whoseTurn = playerTwo.getMark() : whoseTurn = playerOne.getMark();
+
+      return {playerOne, playerTwo}
+
+  })();
+
+  */
 
 })();
